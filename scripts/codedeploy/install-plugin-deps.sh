4342a2a2a261f86ab1d6f962464173f740195607
Steve Yardumian <>
2019-05-03 22:08:52 -0400

    Create the plugins dir if it doesn't exist

--- scripts/codedeploy/install-plugin-deps.sh
+++ scripts/codedeploy/install-plugin-deps.sh
@@ -9,6 +9,7 @@ aws configure set aws_access_key_id AKIA2ZV5MVCVOFMCYKFZ --profile chc
 aws configure set aws_secret_access_key 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 --profile chc
 aws configure set default.region us-east-1 --profile chc
 
+mkdir /var/lib/grafana/plugins 2>/dev/null 
 chmod -R 775 /var/lib/grafana/plugins
 chown -R ec2-user /var/lib/grafana/plugins/
 chgrp -R grafana /var/lib/grafana/plugins/
