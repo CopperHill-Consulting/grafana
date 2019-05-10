b5da3f89ee567d79fd35afffd21f46589d77b43f
Steve Yardumian <syardumian@consultch.com>
2019-05-10 08:57:22 -0400

    Show 'CopperHill AIR' on browser tab title for dashboards

--- public/app/features/dashboard/containers/DashboardPage.tsx
+++ public/app/features/dashboard/containers/DashboardPage.tsx
@@ -105,7 +105,7 @@ export class DashboardPage extends PureComponent<Props, State> {
 
     // if we just got dashboard update title
     if (!prevProps.dashboard) {
-      document.title = dashboard.title + ' - Grafana';
+      document.title = dashboard.title + ' - CopperHill AIR';
     }
 
     // Due to the angular -> react url bridge we can ge an update here with new uid before the container unmounts
