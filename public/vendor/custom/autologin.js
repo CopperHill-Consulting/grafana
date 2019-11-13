8fcdaa7e535b8eed22b46ade23d351310b6e4d57
Steve Yardumian <syardumian@consultch.com>
2019-11-12 21:10:08 -0500

    Default html banner; add cookie recognition for logging out before auto-login

--- public/vendor/custom/autologin.js
+++ public/vendor/custom/autologin.js
@@ -6,35 +6,6 @@ $( document ).ready(function() {
      * @type type
      */
     var helperCookie = {
-        findAll: function () {
-            var cookies = {};
-            _(document.cookie.split(';'))
-                    .chain()
-                    .map(function (m) {
-                        return m.replace(/^\s+/, '').replace(/\s+$/, '');
-                    })
-                    .each(function (c) {
-                        var arr = c.split('='),
-                                key = arr[0],
-                                value = null;
-                        var size = _.size(arr);
-                        if (size > 1) {
-                            value = arr.slice(1).join('');
-                        }
-                        cookies[key] = value;
-                    });
-            return cookies;
-        },
-        find: function (name) {
-            var cookie = null,
-                    list = this.findAll();
-
-            _.each(list, function (value, key) {
-                if (key === name)
-                    cookie = value;
-            });
-            return cookie;
-        },
         create: function (name, value, time) {
             var today = new Date(),
                     offset = (typeof time == 'undefined') ? (1000 * 60 * 60 * 24) : (time * 1000),
@@ -50,18 +21,12 @@ $( document ).ready(function() {
 
             document.cookie = cookie;
             return this;
-        },
-        destroy: function (name, cookie) {
-            if (cookie = this.find(name)) {
-                this.create(name, null, -1000000);
-            }
-            return this;
         }
     };
 
     function getUrlVars() {
         var vars = {};
-        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        const parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
             vars[key] = value;
         });
         return vars;
@@ -75,7 +40,26 @@ $( document ).ready(function() {
         return urlparameter;
     };
 
-    var t = getUrlParameter('t');
+    //check for login info cookie to send us to a login screen
+    const cookieArr = document.cookie.split(';');
+    cookieArr.forEach(function(value, index) {
+        if (value.includes("login_info")) {
+            const valueParts = value.split('=');
+            const loginInfoCookieStr = valueParts[1];
+
+            if (loginInfoCookieStr) {
+                var newLocation = window.location.protocol + '//' + window.location.host + '/login?t=';
+                const data = new Buffer(decodeURIComponent(loginInfoCookieStr), 'utf-8').toString('base64');
+                const t_encoded = encodeURIComponent(data);
+                newLocation += t_encoded;
+                helperCookie.create("login_info", null, -1000000);
+                window.location = newLocation;
+            }
+        }
+    });
+
+    //deal with parameter t
+    const t = getUrlParameter('t');
 
     if (typeof (t) == 'undefined') {
         return false;
@@ -97,11 +81,12 @@ $( document ).ready(function() {
          'z-index': 5000
       });
 
-    try {
-        var t_decoded = decodeURIComponent(t);
-        var dataBuffer = new Buffer(t_decoded, 'base64');
-        var data = dataBuffer.toString('utf-8');
-        var ojson = JSON.parse(data);
+    try{
+
+        const t_decoded = decodeURIComponent(t);
+        const dataBuffer = new Buffer(t_decoded, 'base64');
+        const data = dataBuffer.toString('utf-8');
+        const ojson = JSON.parse(data);
 
         if (typeof (ojson.user) == 'undefined' || typeof (ojson.pass) == 'undefined') {
             throw "Undefined type";
@@ -111,28 +96,31 @@ $( document ).ready(function() {
             throw "User or password empty";
         }
 
-        if (typeof (ojson.clearCookie) != 'undefined'){
-            helperCookie.destroy(clearCookie);
+        if (typeof (ojson.user) != 'undefined' && typeof (ojson.pass) != 'undefined' && typeof (ojson.logout) != 'undefined') {
+            helperCookie.create("login_info", JSON.stringify({ "user": ojson.user, "pass": ojson.pass }));
+            window.location =  window.location.protocol + '//' + window.location.host + '/logout';
         }
 
+        setTimeout(function () {
+            var $form = $('form[name="loginForm"]');
+      
+            if (typeof (ojson.redirect_to) != 'undefined') {
+                  helperCookie.create("redirect_to", ojson.redirect_to);
+                if ($form.length <= 0){
+                  window.location = window.location.protocol + '//' + window.location.host + '/login';
+                }
+            }
+      
+            if ($form.length > 0) {
+              $form.find("input[name=username]").val(ojson.user).trigger("input");
+              $form.find("input[name=password]").val(ojson.pass).trigger("input");
+              $form.find("button[type=submit]").trigger('click');
+            }
+          }, 500);
+        
     } catch (e) {
+        console.error(e);
         return false;
     }
-
-    setTimeout(function () {
-      var $form = $('form[name="loginForm"]');
-
-      if (typeof (ojson.redirect_to) != 'undefined') {
-          helperCookie.create("redirect_to", ojson.redirect_to);
-          if ($form.length <= 0){
-            window.location = window.location.protocol + '//' + window.location.host + '/login';
-          }
-      }
-
-      if ($form.length > 0) {
-        $form.find("input[name=username]").val(ojson.user).trigger("input");
-        $form.find("input[name=password]").val(ojson.pass).trigger("input");
-        $form.find("button[type=submit]").trigger('click');
-      }
-    }, 500);
+    
 });
