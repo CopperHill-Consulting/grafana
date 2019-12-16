5700fcb924bd8c97888fd08675e651ecbf2125af
Steve Yardumian <syardumian@consultch.com>
2019-12-16 17:25:16 -0500

    fixes for auto login logic; put redirect back into t param and save it out as a cookie; only do logout if we are not on the login page; only do form fill and submit if we are on the login page

--- public/vendor/custom/autologin.js
+++ public/vendor/custom/autologin.js
@@ -42,30 +42,31 @@ $( document ).ready(function() {
 
     //check for login info cookie to send us to a login screen
     const cookieArr = document.cookie.split(';');
+    var loginInfoIndex = -1;
     cookieArr.forEach(function(value, index) {
         if (value.includes("login_info")) {
-            const valueParts = value.split('=');
-            const loginInfoCookieStr = valueParts[1];
-
-            if (loginInfoCookieStr) {
-                var newLocation = window.location.protocol + '//' + window.location.host + '/login?t=';
-                const data = new Buffer(decodeURIComponent(loginInfoCookieStr), 'utf-8').toString('base64');
-                const t_encoded = encodeURIComponent(data);
-                newLocation += t_encoded;
-                helperCookie.create("login_info", null, -1000000);
-                window.location = newLocation;
-            }
+            loginInfoIndex = index;
         }
     });
 
-    //deal with parameter t
-    const t = getUrlParameter('t');
+    if (loginInfoIndex != -1) {
+        var cookieParts = cookieArr[loginInfoIndex].split('=');
+        const loginInfoCookieStr = cookieParts[1];
 
-    if (typeof (t) == 'undefined') {
-        return false;
+        if (loginInfoCookieStr) {
+            //console.log('loading login page with T params')
+            var newLocation = window.location.protocol + '//' + window.location.host + '/login?t=';
+            const data = new Buffer(decodeURIComponent(loginInfoCookieStr), 'utf-8').toString('base64');
+            newLocation += data;
+            helperCookie.create("login_info", null, -1000000);
+
+            window.location = newLocation;
+        }
     }
 
-    if (t.length == 0) {
+    //deal with parameter t
+    const t = getUrlParameter('t');
+    if (typeof (t) == 'undefined' || t.length == 0) {
         return false;
     }
 
@@ -97,26 +98,30 @@ $( document ).ready(function() {
         }
 
         if (typeof (ojson.user) != 'undefined' && typeof (ojson.pass) != 'undefined' && typeof (ojson.logout) != 'undefined') {
+            //console.log('creating login info cookie')
             helperCookie.create("login_info", JSON.stringify({ "user": ojson.user, "pass": ojson.pass }));
-            window.location =  window.location.protocol + '//' + window.location.host + '/logout';
+            if (typeof (ojson.redirect) != 'undefined') {
+                //console.log('creating redirect to cookie')
+                helperCookie.create("redirect_to", ojson.redirect );
+            }
+            if (ojson.logout && !window.location.href.includes('/login')) {
+                //console.log('logging out')
+                window.location = window.location.protocol + '//' + window.location.host + '/logout';
+            }
         }
 
-        setTimeout(function () {
-            var $form = $('form[name="loginForm"]');
-      
-            if (typeof (ojson.redirect_to) != 'undefined') {
-                  helperCookie.create("redirect_to", ojson.redirect_to);
-                if ($form.length <= 0){
-                  window.location = window.location.protocol + '//' + window.location.host + '/login';
+        if (window.location.href.includes('/login')){
+            setTimeout(function () {
+                //console.log('submitting login form with T params')
+                var $form = $('form[name="loginForm"]');
+                
+                if ($form.length > 0) {
+                    $form.find("input[name=username]").val(ojson.user).trigger("input");
+                    $form.find("input[name=password]").val(ojson.pass).trigger("input");
+                    $form.find("button[type=submit]").trigger('click');
                 }
-            }
-      
-            if ($form.length > 0) {
-              $form.find("input[name=username]").val(ojson.user).trigger("input");
-              $form.find("input[name=password]").val(ojson.pass).trigger("input");
-              $form.find("button[type=submit]").trigger('click');
-            }
-          }, 500);
+            }, 500);
+        }
         
     } catch (e) {
         console.error(e);
